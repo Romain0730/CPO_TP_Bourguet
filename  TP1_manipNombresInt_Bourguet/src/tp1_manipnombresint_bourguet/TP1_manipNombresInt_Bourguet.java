d832aaecbf9a5a2a82197457f1d8edbb1bdea49f
Romain Bourguet <romai@10.3.62.136>
2025-10-22 17:00:40 +0200

    création d'un nouveau projet versionné

---
+++  TP1_manipNombresInt_Bourguet/src/tp1_manipnombresint_bourguet/TP1_manipNombresInt_Bourguet.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_manipnombresint_bourguet;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author romai
+ */
+public class TP1_manipNombresInt_Bourguet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Entrez le premier entier : ");
+        int nombre1 = scanner.nextInt();
+
+        System.out.print("Entrez le deuxième entier : ");
+        int nombre2 = scanner.nextInt();
+
+        System.out.println("Vous avez saisi : " + nombre1 + " et " + nombre2);
+
+        scanner.close();
+    }
+}
+        
+        // TODO code application logic here
+    }
+    
+}
