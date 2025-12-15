e8a514d027e53b559fd7ec0cd459245dd3936c68
Romain Bourguet <romai@10.3.62.136>
2025-12-15 17:16:39 +0100

    seance 15/12 fin . classe Cellule lumineuse

---
+++ LightOff_BOURGUET_version_console/src/CelluleLumineuse.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author romai
+ */
+public class CelluleLumineuse {
+    private boolean etat;
+
+    public CelluleLumineuse() {
+        this.etat = false;
+    }
+    public void activerCellule() {
+        etat = !etat;
+    }
+    public void eteindreCellule() {
+        etat = false;
+    }
+    public boolean estEteint() {
+        return etat == false;
+    }
+    public boolean getEtat() {
+        return etat;
+    }
+    @Override
+    public String toString() {
+        if (etat) {
+            return "X"; 
+        } else {
+            return "O"; 
+        }
+    }
+}
