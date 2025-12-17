c2b488842a9fb2e719abc59d2f7e7684fac82396
Romain Bourguet <romai@10.3.62.136>
2025-12-17 11:40:02 +0100

    correction nom package

---
+++ LightOff_BOURGUET_version_console/src/lightoff_bourguet_version_console/CelluleLumineuse.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_bourguet_version_console;
+
+/**
+ *
+ * @author romai
+ */
+public class CelluleLumineuse {
+    private boolean etat;
+
+    public CelluleLumineuse() {
+        this.etat = false;
+    }
+    public void activerCellule() {
+        etat = !etat;
+    }
+    public void eteindreCellule() {
+        etat = false;
+    }
+    public boolean estEteint() {
+        return etat == false;
+    }
+    public boolean getEtat() {
+        return etat;
+    }
+    @Override
+    public String toString() {
+        if (etat) {
+            return "1"; 
+        } else {
+            return "0"; 
+        }
+    }
+}
