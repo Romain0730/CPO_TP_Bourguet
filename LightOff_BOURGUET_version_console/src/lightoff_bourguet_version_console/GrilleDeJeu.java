bc15d7f1f6955db5f0d1aa7e77a99f69079446c7
Romain Bourguet <romai@10.3.62.136>
2025-12-19 12:01:18 +0100

    classe grille de jeu fonctionelle avec les test dans le main. passage à la question 7

---
+++ LightOff_BOURGUET_version_console/src/lightoff_bourguet_version_console/GrilleDeJeu.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_bourguet_version_console;
+import java.util.Random;
+/**
+ *
+ * @author romai
+ */
+public class GrilleDeJeu {
+    private CelluleLumineuse[][] matriceCellules;
+    private int nbLignes;
+    private int nbColonnes;
+
+   
+    
+    
+    public GrilleDeJeu(int p_nbLignes, int p_nbColonnes) {
+        nbLignes = p_nbLignes;
+        nbColonnes = p_nbColonnes;
+        matriceCellules = new CelluleLumineuse[nbLignes][nbColonnes];
+
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                matriceCellules[i][j] = new CelluleLumineuse();
+            }
+        }
+    }
+    public void activerLigneDeCellules(int idLigne){
+     for (int col = 0; col < nbColonnes; col++) {
+        matriceCellules[idLigne][col].activerCellule();
+      }
+    }
+    public void activerColonneDeCellules(int idColonne){
+        for (int lig = 0; lig < nbLignes; lig++) {
+        matriceCellules[lig][idColonne].activerCellule();
+    }
+    }
+    public void activerDiagonaleDescendante() {
+         for (int i = 0; i < Math.min(nbLignes, nbColonnes); i++) {
+        matriceCellules[i][i].activerCellule();
+      }
+    }
+    public void activerDiagonaleMontante(){
+        for (int i = 0; i < Math.min(nbLignes, nbColonnes); i++) {
+        matriceCellules[i][nbColonnes - 1 - i].activerCellule();
+      }
+    }
+    public void eteindreToutesLesCellules() {
+    for (int i = 0; i < nbLignes; i++) {
+        for (int j = 0; j < nbColonnes; j++) {
+            matriceCellules[i][j].eteindreCellule();
+        }
+    }
+}
+    public boolean cellulesToutesEteintes() {
+    for (int i = 0; i < nbLignes; i++) {
+        for (int j = 0; j < nbColonnes; j++) {
+            if (!matriceCellules[i][j].estEteint()) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+    public void activerLigneColonneOuDiagonaleAleatoire(){
+        Random rand = new Random();
+    int choix = rand.nextInt(4);
+
+    switch (choix) {
+        case 0 -> activerLigneDeCellules(rand.nextInt(nbLignes));
+        case 1 -> activerColonneDeCellules(rand.nextInt(nbColonnes));
+        case 2 -> activerDiagonaleDescendante();
+        case 3 -> activerDiagonaleMontante();
+    }
+      }
+    public void melangerMatriceAleatoirement(int nbTours){
+        eteindreToutesLesCellules();
+    for (int i = 0; i < nbTours; i++) {
+        activerLigneColonneOuDiagonaleAleatoire();
+      }
+    }
+    @Override
+    public String toString(){
+        String resultat ="";
+        for (int i = 0 ; i <nbLignes; i++){
+            for(int j = 0 ; j< nbColonnes; j++){
+            resultat += matriceCellules[i][j];
+        }
+        resultat += "\n";
+    }
+    return resultat;
+   }
+}
+    
