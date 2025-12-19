4d1df639c878e8d62fc44518c49897ed7347016c
Romain Bourguet <romai@10.3.62.136>
2025-12-19 17:15:18 +0100

    classe Partie terminé

--- LightOff_BOURGUET_version_console/src/lightoff_bourguet_version_console/LightOff_BOURGUET_version_console.java
+++ LightOff_BOURGUET_version_console/src/lightoff_bourguet_version_console/LightOff_BOURGUET_version_console.java
@@ -14,28 +14,8 @@ public class LightOff_BOURGUET_version_console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       GrilleDeJeu grille = new GrilleDeJeu(5, 5);
-        System.out.println("Grille initiale :");
-        System.out.println(grille);
-
-        grille.activerLigneDeCellules(2);
-        System.out.println("Après activation de la ligne 2 :");
-        System.out.println(grille);
-
-        grille.activerColonneDeCellules(1);
-        System.out.println("Après activation de la colonne 1 :");
-        System.out.println(grille);
-
-        grille.activerDiagonaleDescendante();
-        System.out.println("Après activation de la diagonale descendante :");
-        System.out.println(grille);
-
-        grille.activerDiagonaleMontante();
-        System.out.println("Après activation de la diagonale montante :");
-        System.out.println(grille);
-
-        grille.melangerMatriceAleatoirement(10);
-        System.out.println("Après mélange aléatoire :");
-        System.out.println(grille);
+      
+Partie p = new Partie();
+        p.lancerPartie();
     }
 }
