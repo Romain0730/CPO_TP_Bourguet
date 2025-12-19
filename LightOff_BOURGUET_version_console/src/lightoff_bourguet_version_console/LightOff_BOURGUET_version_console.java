937d5a5024fad9edcbf03a5eedc9aa2c4903ba20
Romain Bourguet <romai@10.3.62.136>
2025-12-19 12:00:32 +0100

    classe grille de jeu fonctionelle avec les test dans le main. passage à la question 7

--- LightOff_BOURGUET_version_console/src/lightoff_bourguet_version_console/LightOff_BOURGUET_version_console.java
+++ LightOff_BOURGUET_version_console/src/lightoff_bourguet_version_console/LightOff_BOURGUET_version_console.java
@@ -14,30 +14,32 @@ public class LightOff_BOURGUET_version_console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       CelluleLumineuse Cellule1 = new CelluleLumineuse();
-       CelluleLumineuse Cellule2 = new CelluleLumineuse();
-       
-       System.out.println("etat initial des cellules :");
-       System.out.println("Cellule 1 : " + Cellule1);
-       System.out.println("Cellule 2 : " + Cellule2);
-       Cellule1.activerCellule();
-       Cellule1.eteindreCellule();
-       
-       System.out.println("\netat après modification :");
-       System.out.println("Cellule 1 : " + Cellule1);
-       System.out.println("Cellule 2 : " + Cellule2);
-       
-       System.out.println("\nTests des méthodes :");
-       System.out.println("Cellule 1 est eteinte ? " + Cellule1.estEteint());
-       System.out.println("Cellule 2 est eteinte ? " + Cellule2.estEteint());
-       
-       System.out.println("etat brut cellule 1 (getEtat) : " + Cellule1.getEtat());
-       System.out.println("etat brut cellule 2 (getEtat) : " + Cellule2.getEtat());
-       
-       GrilleDeJeux grille = new GrilleDeJeux(3);
+       GrilleDeJeu grille = new GrilleDeJeu(5, 5);
+        System.out.println("Grille initiale :");
+        System.out.println(grille);
 
-    System.out.println("Affichage de la grille :");
-    System.out.println(grille);
+        // Test e : activer une ligne
+        grille.activerLigneDeCellules(2);
+        System.out.println("Après activation de la ligne 2 :");
+        System.out.println(grille);
+
+        // Test e : activer une colonne
+        grille.activerColonneDeCellules(1);
+        System.out.println("Après activation de la colonne 1 :");
+        System.out.println(grille);
+
+        // Test e : diagonales
+        grille.activerDiagonaleDescendante();
+        System.out.println("Après activation de la diagonale descendante :");
+        System.out.println(grille);
+
+        grille.activerDiagonaleMontante();
+        System.out.println("Après activation de la diagonale montante :");
+        System.out.println(grille);
+
+        // Test e : mélange aléatoire
+        grille.melangerMatriceAleatoirement(10);
+        System.out.println("Après mélange aléatoire :");
+        System.out.println(grille);
     }
-    
 }
