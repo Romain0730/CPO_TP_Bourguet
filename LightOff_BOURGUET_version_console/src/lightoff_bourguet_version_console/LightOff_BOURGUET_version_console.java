03c8a0d023cb9b9972e56bdda3a1f670c41c0ce8
Romain Bourguet <romai@10.3.62.136>
2025-12-24 00:53:51 +0100

    lien entre interface et jeu avec "1" et "0". Comit car netBeans bug

--- LightOff_BOURGUET_version_console/src/lightoff_bourguet_version_console/LightOff_BOURGUET_version_console.java
+++ LightOff_BOURGUET_version_console/src/lightoff_bourguet_version_console/LightOff_BOURGUET_version_console.java
@@ -15,7 +15,8 @@ public class LightOff_BOURGUET_version_console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      //
+     
+        //
 Partie p = new Partie();
         p.lancerPartie();
     }
